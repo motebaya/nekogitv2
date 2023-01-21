24624897f2760e28fc442c3d123d6d1586da6b95
It's daviana <106154800+motebaya@users.noreply.github.com>
2023-01-21 18:31:04 +0700

    miss indent

--- test/tes.py
+++ test/tes.py
@@ -3,8 +3,8 @@
 from nekov2 import NekoV2
 
 def save(f, to):
-	with open(to, 'w') as w:
-		w.write(f)
+    with open(to, 'w') as w:
+        w.write(f)
 
 # sample call
 h = list(NekoV2().get_hentai_list())
@@ -12,7 +12,7 @@ save(str(h), 'test/all_hentai_list.json')
 h = NekoV2().get_hentai_info(h[0])
 save(str(h), 'test/hentai_info.json')
 h = NekoV2().get_download(
-	h['eps'][0]['1']
+    h['eps'][0]['1']
 )
 save(str(h), 'test/download.json')
 
